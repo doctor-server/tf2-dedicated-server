38b021172fc6ce6221631b690644b13d17882f5a
TatLead <hkdsethomas@gmail.com>
2024-11-03 21:02:09 +0800

    Update validate_buildid.sh

--- validate_buildid.sh
+++ validate_buildid.sh
@@ -6,7 +6,7 @@
 # the script exits with an error message. If they match, it confirms the match.
 
 # Extract the local build ID from the appmanifest file
-local_buildid=$(grep -oP '"buildid".{1,}"(.*?)"' /home/steam/serverfiles/steamapps/appmanifest_$1.acf | sed -n 's/.*"buildid".*"\(.*\)"/\1/p')
+local_buildid=$(grep -oP '"buildid".{1,}"(.*?)"' $HOME/serverfiles/steamapps/appmanifest_$1.acf | sed -n 's/.*"buildid".*"\(.*\)"/\1/p')
 
 # Compare the local build ID with the remote build ID
 if [ "$local_buildid" != "$2" ]; then
